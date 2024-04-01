141566c7a0c55aff1fa55d207647174ef363e04f
Vineeth Chandran <vineeth.chandran@toobler.com>
2024-04-01 12:20:22 +0530

    menu added in init

--- lib/commands/init/init.dart
+++ lib/commands/init/init.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:dcli/dcli.dart';
+import 'package:easy_init_cli/common/utils/ask_question.dart';
 import 'package:easy_init_cli/common/utils/shell_utils.dart';
 import 'package:easy_init_cli/core/file_contents.dart';
 import 'package:easy_init_cli/core/structure.dart';
@@ -15,10 +16,24 @@ class InitProject extends Command with Logging {
 
   @override
   Future<void> excecute() async {
+    var choice = UserInput.menu(
+        options: ["TDD Clean Architecture"], promt: "Choose architecture");
+
     blueLog("Initializing your project...");
     print("");
     blueLog(logo);
     print('');
+    switch (choice) {
+      case 1:
+        await initTddClean();
+        break;
+      default:
+        print("Choice not found");
+        break;
+    }
+  }
+
+  Future<void> initTddClean() async {
     List<Directory> directories =
         Structure.cleanArchitectureStructure.values.toList();
     List<Directory> homeFeatureDirectories =
