57a625e505185bcd8075f0c1e17669b32f3f40d2
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2024-04-02 19:59:37 +0530

    version 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 
+## 1.0.1
+ bug fix
+
 ## 1.0.0
  Initial release.
 - Create flutter projec
--- lib/core/structure/tdd_clean_structure/file_contents.dart
+++ lib/core/structure/tdd_clean_structure/file_contents.dart
@@ -17,7 +17,7 @@ String logo = '''
       ####  ##      ##  ####      ##
 
          Made with 🩵  by Vineeth
-              Version :1.0.0
+              Version :1.0.1
 
 ''';
 
