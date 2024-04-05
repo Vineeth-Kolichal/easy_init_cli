ac86d4c66c5f9d12ae1a68e3b2e691c478e1e1ae
Vineeth Chandran <vineeth.chandran@toobler.com>
2024-04-05 16:37:32 +0530

    file contents modified

--- lib/core/structure/tdd_clean_structure/file_contents/common_content.dart
+++ lib/core/structure/tdd_clean_structure/file_contents/common_content.dart
@@ -1,14 +1,6 @@
 import 'package:easy_init_cli/commands/create_feature/create_feature.dart';
 import 'package:recase/recase.dart';
 
-String apiEndpointContent = '''
-class ApiEndpoints {
-  /// change the [baseUrl] value as per your api
-  static String baseUrl = "http://numbersapi.com/";
-}
-
-''';
-
 String usecaseContent = '''
 import 'package:dartz/dartz.dart';
 import '../failures/failures.dart';
@@ -33,18 +25,6 @@ Future<void> configureInjection() async {
 }
 ''';
 
-String dioModuleContent = '''
-import 'package:dio/dio.dart';
-import 'package:injectable/injectable.dart';
-import '../../api_endpoints/api_endpoints.dart';
-
-@module
-abstract class DioModule {
-  @lazySingleton
-  Dio get dioInstance => Dio(BaseOptions(baseUrl: ApiEndpoints.baseUrl));
-}
-''';
-
 String failuresContent = '''
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'failures.freezed.dart';
@@ -53,77 +33,6 @@ part 'failures.freezed.dart';
 class Failure with _\$Failure {
   factory Failure.apiRequestFailure(String error) = ApiRequestFailure;
 }
-''';
-
-String networkExceptionContent = '''
-import 'package:dio/dio.dart';
-
-class CustomException implements Exception {
-  CustomException.otherException(String msg) {
-    message = msg;
-  }
-  CustomException.fromDioError(DioException dioError) {
-    if (message == null) {
-      switch (dioError.type) {
-        case DioException.requestCancelled:
-          message = "Request to API server was cancelled";
-          break;
-        case DioException.connectionTimeout:
-          message = "Connection timeout with API server";
-          break;
-        case DioException.connectionError:
-          message =
-              "Connection to API server failed due to internet connection";
-          break;
-        case DioException.receiveTimeout:
-          message = "Receive timeout in connection with API server";
-          break;
-        case DioException.badResponse:
-          message = _handleError(dioError.response!.statusCode);
-          break;
-        case DioException.sendTimeout:
-          message = "Send timeout in connection with API server";
-          break;
-        default:
-          message = _handleDefaultError(dioError);
-          break;
-      }
-    }
-  }
-
-  dynamic message;
-
-  String? _handleDefaultError(DioException error) {
-    try {
-      final response = error.response?.data as Map?;
-      if (response != null) {
-        return "\${response["message"]}";
-      }
-      return error.message;
-    } catch (_) {
-      return error.message;
-    }
-  }
-
-  Object _handleError(statusCode) {
-    switch (statusCode) {
-      case 400:
-        return 'Bad request';
-      case 401:
-        return 'Unauthorized request';
-      case 404:
-        return 'The requested resource was not found';
-      case 500:
-        return 'Internal server error';
-      default:
-        return 'Something went wrong';
-    }
-  }
-
-  @override
-  String toString() => message.toString();
-}
-
 ''';
 
 String routesContent = '''
