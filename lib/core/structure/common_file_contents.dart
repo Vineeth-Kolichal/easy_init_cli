00196094db0a1c38d89d3ec66afba84978945298
Vineeth Chandran <vineeth.chandran@toobler.com>
2024-04-17 14:37:57 +0530

    feat: network client implementation

--- lib/core/structure/common_file_contents.dart
+++ lib/core/structure/common_file_contents.dart
@@ -98,7 +98,7 @@ class CustomException implements Exception {
   CustomException.otherException(String msg) {
     message = msg;
   }
-  CustomException.fromDioError(DioException dioError) {
+  CustomException.fromDioException(DioException dioError) {
     if (message == null) {
       switch (dioError.type) {
         case DioException.requestCancelled:
@@ -231,4 +231,140 @@ class OtpScreen extends StatelessWidget {
     return Scaffold();
   }
 }
+''';
+
+String networkClientContent = '''
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+
+import 'network_exceptions.dart';
+
+/// If you have to pass token with api requests then use,[getWithToken],[postWithToken],
+/// [putWithToken],[patchWithToken],[deleteWithToken], methods, if you are not using any token then
+/// you can use [getWithoutToken] and [postWithoutToken] methods. You can implement Put,Patch,delete
+/// without token  methods as per your needs
+/// You can modify this code as per your needs.
+
+@lazySingleton
+@injectable
+class NetworkClient {
+  final Dio _dio;
+  NetworkClient(this._dio);
+
+  ///If you are storing token in SharedPreferences or any other storage,
+  ///then write code for retrieving token and assign to [token]
+  String? token;
+  //Get request with token
+  Future<dynamic> getWithToken(
+      {required String path, dynamic data, dynamic queryParameters}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+      "authorization": "Bearer \$token"
+    };
+    try {
+      final response =
+          await _dio.get(path, data: data, queryParameters: queryParameters);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    }
+  }
+
+  //Post request with token
+  Future<dynamic> postWithToken({required String path, dynamic data}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+      "authorization": "Bearer \$token"
+    };
+    try {
+      final response = await _dio.post(path, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    } catch (e) {
+      throw CustomException.otherException(e.toString());
+    }
+  }
+
+  //Put request with token
+  Future<dynamic> putWithToken({required String path, dynamic data}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+      "authorization": "Bearer \$token"
+    };
+    try {
+      final response = await _dio.put(path, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    } catch (e) {
+      throw CustomException.otherException(e.toString());
+    }
+  }
+
+  //Patch request with token
+  Future<dynamic> patchWithToken({required String path, dynamic data}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+      "authorization": "Bearer \$token"
+    };
+    try {
+      final response = await _dio.patch(path, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    } catch (e) {
+      throw CustomException.otherException(e.toString());
+    }
+  }
+
+  //Delete request with token
+  Future<dynamic> deleteWithToken({required String path, dynamic data}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+      "authorization": "Bearer \$token"
+    };
+    try {
+      final response = await _dio.delete(path, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    } catch (e) {
+      throw CustomException.otherException(e.toString());
+    }
+  }
+
+  //Get request without token
+  Future<dynamic> getWithoutToken(
+      {required String path, dynamic data, dynamic queryParameters}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+    };
+    try {
+      final response =
+          await _dio.get(path, data: data, queryParameters: queryParameters);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    } catch (e) {
+      throw CustomException.otherException(e.toString());
+    }
+  }
+
+  //Post request without token
+  Future<dynamic> postWithoutToken({required String path, dynamic data}) async {
+    _dio.options.headers = {
+      "Content-Type": "application/json",
+    };
+    try {
+      final response = await _dio.post(path, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw CustomException.fromDioException(e);
+    } catch (e) {
+      throw CustomException.otherException(e.toString());
+    }
+  }
+}
+
 ''';
--- lib/core/structure/tdd_clean_structure/file_contents/number_trivia_content.dart
+++ lib/core/structure/tdd_clean_structure/file_contents/number_trivia_content.dart
@@ -1,35 +1,38 @@
 const triviaDataSourceContent = '''
-import 'package:dio/dio.dart'; // Importing Dio for making HTTP requests
-import 'package:injectable/injectable.dart'; // Importing injectable for dependency injection
+import 'package:injectable/injectable.dart';
 
-import '../../../../core/network_exceptions/network_exceptions.dart'; // Importing custom network exceptions
-import '../../domain/usecases/get_number_trivia_usecase.dart'; // Importing domain use case
-import '../models/trivia_model.dart'; // Importing trivia model
+import '../../../../core/network_exceptions/network_client.dart';
+import '../../domain/usecases/get_number_trivia_usecase.dart';
+import '../models/trivia_model.dart';
+
+//This is a sample datasource file
 
 // Abstract class representing a data source for fetching number trivia
 abstract class NumberTriviaDataSource {
   Future<TriviaModel> getConcreteTrivia(
       NumberParam params); // Method for getting concrete trivia
 }
+
 // Registering NumberTriviaDataSource as a lazy singleton for dependency injection
-@LazySingleton(as: NumberTriviaDataSource) 
+@LazySingleton(as: NumberTriviaDataSource)
 @injectable
 class NumberTriviaDataSourceImpl implements NumberTriviaDataSource {
-  final Dio dio; // Dio instance for making HTTP requests
+  final NetworkClient client;
 
-  NumberTriviaDataSourceImpl(this.dio); // Constructor injecting Dio dependency
+  NumberTriviaDataSourceImpl(
+      this.client); // Constructor injecting NetworClient dependency
 
   @override
   Future<TriviaModel> getConcreteTrivia(NumberParam params) async {
     try {
       // Making GET request to fetch trivia
-      final response = await dio.get("/\${params.number}/trivia?json");
+      final response =
+          await client.getWithoutToken(path: "/\${params.number}/trivia?json");
       // Parsing response data into TriviaModel
       return TriviaModel.fromJson(response.data);
-    } on DioException catch (e) {
-      // Handling Dio errors
-      // Throwing custom exception based on DioError
-      throw CustomException.fromDioError(e);
+    } catch (e) {
+      //throwing exception
+      rethrow;
     }
   }
 }
