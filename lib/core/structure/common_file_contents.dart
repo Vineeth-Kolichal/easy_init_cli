3ae0c930c662aacf791ffbabe5028284ea415690
Vineeth-Kolichal <vineeth.chandran@toobler.com>
2024-08-17 22:09:24 +0530

    unwanted log removed

--- lib/core/structure/common_file_contents.dart
+++ lib/core/structure/common_file_contents.dart
@@ -986,7 +986,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FcmHelper {
-  ///<<<<<<<<<<<<<<<<TODO>>>>>>>>>>>>>>>>>>>>>>
+  //TODO:!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   /// //init notification
   /// call initNotifications() method to initialize notifications
   /// ```
