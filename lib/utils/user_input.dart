71e592137733661983a5bfe410abe39c728245e1
Vineeth Chandran <vineeth.chandran@toobler.com>
2026-01-07 18:03:42 +0530

    refactor: Simplify user input and menu logic using dcli functions.

--- lib/utils/user_input.dart
+++ lib/utils/user_input.dart
@@ -1,36 +1,19 @@
-import 'dart:io';
-
-import 'package:dcli/dcli.dart';
+import 'package:dcli/dcli.dart' as dcli;
 
 class UserInput {
   static String askQuestion(String question, String example) {
-    print("${yellow("$question?")} ${grey("example: $example")}");
-    final ans = stdin.readLineSync();
-    if (ans != null && ans.isNotEmpty) {
-      return ans;
-    } else {
-      return askQuestion(question, example);
-    }
+    return dcli.ask(
+      "${dcli.yellow(question)} ${dcli.grey("($example)")}",
+      validator: dcli.Ask.required,
+    );
   }
 
   static int menu({required String prompt, required List<String> options}) {
-    print('');
-    for (var i = 0; i < options.length; i++) {
-      print(yellow("  ${i + 1}. ${options[i]}"));
-    }
-    print('');
-    print(blue(prompt));
-    var ans = stdin.readLineSync();
-    if (ans == null) {
-      return menu(prompt: prompt, options: options);
-    } else {
-      var s = int.parse(ans);
-      if (s == 0 || s > options.length) {
-        print(red("[ERROR] Invalid selection!"));
-        return menu(prompt: prompt, options: options);
-      } else {
-        return s;
-      }
-    }
+    final selection = dcli.menu(
+      dcli.blue(prompt),
+      options: options,
+      defaultOption: options.first,
+    );
+    return options.indexOf(selection) + 1;
   }
 }
